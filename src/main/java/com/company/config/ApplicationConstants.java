2f1c396d01629081764839ef358e96bf0a83262a
Kanan Akhmadli <Kanan.Ahmadli@kapitalbank.az>
2021-11-27 03:07:18 +0400

    Resolving bugs and updating

---
+++ src/main/java/com/company/config/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.company.config;
+
+public interface ApplicationConstants {
+
+    String SPRING_PROFILE_LOCAL = "local";
+    String SPRING_PROFILE_DEV = "dev";
+    String SPRING_PROFILE_PREPROD = "preprod";
+    String SPRING_PROFILE_PROD = "prod";
+
+}
---
+++ src/main/java/com/company/error/ErrorHandler.java
@@ -0,0 +1,11 @@
+//package com.company.error;
+//
+//import az.kapitalbank.front.lib.error.handler.CommonErrorHandler;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@Slf4j
+//@RestControllerAdvice
+//public class ErrorHandler extends CommonErrorHandler {
+//
+//}
